cd0d31d99d60ec258a21ee35a56d5e532ec489cf
Esa Permana <esapermanami24@gmail.com>
2026-01-13 18:28:28 +0700

    update pengelola to admin

---
+++ lib/pengelola/screens/place_model.dart
@@ -0,0 +1,17 @@
+class Place {
+  String id;
+  String name;
+  String location;
+  String category;
+  String price;
+  String description;
+
+  Place({
+    required this.id,
+    required this.name,
+    required this.location,
+    required this.category,
+    required this.price,
+    required this.description,
+  });
+}
